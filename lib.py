d9e42f925d5a7d00384e19920e6ac71ee5205239
VaibhaviNandurkar3 <vaibhavinandurkar2401@gmail.com>
2026-03-27 08:02:55 +0530

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,74 @@
+# Simple Library System using Dictionary
+
+library = {}  # This is our hash table
+
+
+# Add Book
+def add_book():
+    isbn = input("Enter ISBN: ")
+    title = input("Enter book title: ")
+    author = input("Enter author name: ")
+
+    library[isbn] = {"title": title, "author": author}
+
+    print("Book added!\n")
+
+
+# Search Book
+def search_book():
+    isbn = input("Enter ISBN to search: ")
+
+    if isbn in library:
+        print("Book found!")
+        print("Title:", library[isbn]["title"])
+        print("Author:", library[isbn]["author"])
+    else:
+        print("Book not found.")
+    print()
+
+
+# Delete Book
+def delete_book():
+    isbn = input("Enter ISBN to delete: ")
+
+    if isbn in library:
+        del library[isbn]
+        print("Book deleted.")
+    else:
+        print("Book not found.")
+    print()
+
+
+# Show All Books
+def show_books():
+    if len(library) == 0:
+        print("Library is empty.\n")
+    else:
+        print("All books in library:")
+        for isbn in library:
+            print(isbn, "->", library[isbn])
+    print()
+
+
+# Main Menu
+while True:
+    print("1. Add Book")
+    print("2. Search Book")
+    print("3. Delete Book")
+    print("4. Show Books")
+    print("5. Exit")
+
+    choice = input("Enter choice: ")
+
+    if choice == '1':
+        add_book()
+    elif choice == '2':
+        search_book()
+    elif choice == '3':
+        delete_book()
+    elif choice == '4':
+        show_books()
+    elif choice == '5':
+        break
+    else:
+        print("Wrong choice, try again.\n")
